35d056e189334c065269f8c1eebc33cb8f04ecaa
thuhiendangyew07-ux <thuhiendangyew07@gmail.com>
2026-06-16 12:30:51 +0700

    Fix image tag in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.
+st.# laidonlaikep <img width="374" height="455" alt="image" src="https://github.com/user-attachments/assets/1c119cf7-2fff-4650-8d30-754f5d3279e8" />
 # Tiêu đề ứng dụng
 st.title("Ứng dụng tính tiền gửi tiết kiệm_Lê Thị Hiền")
 
