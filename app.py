93a93fdf6e37898cebc230902510713bcfc70a8c
thuhiendangyew07-ux <thuhiendangyew07@gmail.com>
2026-06-16 12:50:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.# laidonlaikep <img width="374" height="455" alt="image" src="https://github.com/user-attachments/assets/1c119cf7-2fff-4650-8d30-754f5d3279e8" />
+st.image("https://drive.google.com/file/d/1vApbqzyzzRQqpRgFliFMyn7gwoRNK3JU/view?usp=drivesdk")
 # Tiêu đề ứng dụng
 st.title("Ứng dụng tính tiền gửi tiết kiệm_Lê Thị Hiền")
 
